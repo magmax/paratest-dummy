e4541a93fe326cda7c47a13cfe4ac7b30ea52262
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2016-11-18 08:39:19 +0100

    changing manifest

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
 
-version = "0.0.1"
+version = "0.0.2"
 name = 'paratest-dummy'
 description = "Test paralelizer for testing (does nothing)"
 module = 'paratest-dummy.dummy'
@@ -74,10 +74,12 @@ setup(
     cmdclass={'test': PyTest},
     classifiers=[
         'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        # 'Programming Language :: Python :: 3.6',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
