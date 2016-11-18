3d26f0bde988088c3bf6bf693737e0ceb3719d71
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2016-11-18 18:03:31 +0100

    increase version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
 
-version = "0.0.5"
+version = "0.0.6"
 name = 'paratest-dummy'
 description = "Test paralelizer for testing (does nothing)"
 module = 'paratest-dummy.dummy'
